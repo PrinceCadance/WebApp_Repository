65aa1c490fdfdf32604c3584ab2eb26b7179aa08
Student <RM207S@campus.qcc.edu>
2019-02-28 21:48:27 -0500

    Chapter 4 lab

---
+++ Chapter4Lab/app.js
@@ -0,0 +1,8 @@
+var main = function() { 
+	"use strict";
+
+	window.alert("Hello world!");
+}
+
+$(document).ready(main); //jquery version of onload event 
+//document ready when html is loaded, window.load means html AND images and files are ready
