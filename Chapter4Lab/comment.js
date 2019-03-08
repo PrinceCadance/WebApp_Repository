0a6cadba17fc95e6cf23af0679a513989b6156ba
Student <RM207S@campus.qcc.edu>
2019-03-07 21:44:35 -0500

    added keypress to chapter 4 comment example

--- Chapter4Lab/comment.js
+++ Chapter4Lab/comment.js
@@ -1,13 +1,28 @@
 "use strict";
 
 $( function()  {
+	var $commentInput = $(".comment-input input");
+
+	function processCommentInput() {
+		var $new_comment;
+		
+
+		$commentInput.focus();
+
+		if ($commentInput.val() !== "") {
+			$new_comment = $("<p>").text($commentInput.val());
+			$(".comments").append($new_comment);
+			$commentInput.val("");			
+		}
+	} // end processCommentInput
+
+	$($commentInput).on("keypress", function(event) {
+		if (event.keyCode === 13) {
+			processCommentInput();
+		}
+	}); // Eend on Keypress
 
 	$(".comment-input button").on("click", function(event) {
-		var $new_comment; 
-
-		if ($(".comment-input input").val() !== "") {
-			$new_comment = $("<p>").text($(".comment-input input").val());
-			$(".comments").append($new_comment);
-		} 		
-	});
+		 processCommentInput();				
+	}); // End on click 
 });
